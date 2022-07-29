b6f0b6923bdfbaa338706a93269f787149445745
Yumika Shiba <yumika.shiba0210@gmail.com>
2022-07-29 08:14:12 -0400

    Update make_aa_feat_internal_modified.m

--- Community Detection/make_aa_feat_internal_modified.m
+++ Community Detection/make_aa_feat_internal_modified.m
@@ -26,34 +26,34 @@ for residue = 1:length(residues)
 	fragment = seq(left_margin : right_margin);
     
 	% 20 aa relative frequencies
-	for j = 1 : 20
-	    vect_aa(residue, 28 * (w - 1) + j) = length(find(fragment == AminoAcids(j))) / length(fragment);
-	    features{28 * (w - 1) + j} = 'AA_frequency';
-	    feat_wins(28 * (w - 1) + j) = windows_aa(w);
-	end
+	% for j = 1 : 20
+	%     vect_aa(residue, 28 * (w - 1) + j) = length(find(fragment == AminoAcids(j))) / length(fragment);
+	%     features{28 * (w - 1) + j} = 'AA_frequency';
+	%     feat_wins(28 * (w - 1) + j) = windows_aa(w);
+	% end
 	% entropy and generalized entropy
 	vect_aa(residue, 28 * (w - 1) + 21) = entropy(vect_aa(residue, 28 * (w - 1) + 1 : 28 * (w - 1) + 20));
 	features{28 * (w - 1) + 21} = 'Entropy';
 	feat_wins(28 * (w - 1) + 21) = windows_aa(w);
 
-	vect_aa(residue, 28 * (w - 1) + 22) = beta_entropy(vect_aa(residue, 28 * (w - 1) + 1 : 28 * (w - 1) + 20), 1.25);
-	features{28 * (w - 1) + 22} = 'Beta_entropy_1.25';
-	feat_wins(28 * (w - 1) + 22) = windows_aa(w);
+	% vect_aa(residue, 28 * (w - 1) + 22) = beta_entropy(vect_aa(residue, 28 * (w - 1) + 1 : 28 * (w - 1) + 20), 1.25);
+	% features{28 * (w - 1) + 22} = 'Beta_entropy_1.25';
+	% feat_wins(28 * (w - 1) + 22) = windows_aa(w);
 	
-	vect_aa(residue, 28 * (w - 1) + 23) = beta_entropy(vect_aa(residue, 28 * (w - 1) + 1 : 28 * (w - 1) + 20), 1.50);
-	features{28 * (w - 1) + 23} = 'Beta_entropy_1.50';
-	feat_wins(28 * (w - 1) + 23) = windows_aa(w);
+	% vect_aa(residue, 28 * (w - 1) + 23) = beta_entropy(vect_aa(residue, 28 * (w - 1) + 1 : 28 * (w - 1) + 20), 1.50);
+	% features{28 * (w - 1) + 23} = 'Beta_entropy_1.50';
+	% feat_wins(28 * (w - 1) + 23) = windows_aa(w);
 
-	vect_aa(residue, 28 * (w - 1) + 24) = beta_entropy(vect_aa(residue, 28 * (w - 1) + 1 : 28 * (w - 1) + 20), 1.75);
-	features{28 * (w - 1) + 24} = 'Beta_entropy_1.75';
-	feat_wins(28 * (w - 1) + 24) = windows_aa(w);
+	% vect_aa(residue, 28 * (w - 1) + 24) = beta_entropy(vect_aa(residue, 28 * (w - 1) + 1 : 28 * (w - 1) + 20), 1.75);
+	% features{28 * (w - 1) + 24} = 'Beta_entropy_1.75';
+	% feat_wins(28 * (w - 1) + 24) = windows_aa(w);
 	
 	% net and total charge
 	pos_charge = length(find(fragment == 'K' | fragment == 'R'));
 	neg_charge = length(find(fragment == 'D' | fragment == 'E'));
 	vect_aa(residue, 28 * (w - 1) + 25) = (pos_charge - neg_charge)/ length(fragment);
-	features{28 * (w - 1) + 25} = 'Net_charge';
-        feat_wins(28 * (w - 1) + 25) = windows_aa(w);
+	% features{28 * (w - 1) + 25} = 'Net_charge';
+        % feat_wins(28 * (w - 1) + 25) = windows_aa(w);
 
 	vect_aa(residue, 28 * (w - 1) + 26) = (pos_charge + neg_charge)/ length(fragment);
 	features{28 * (w - 1) + 26} = 'Total_charge';
